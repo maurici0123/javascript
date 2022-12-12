61df58a22dd0b89d900a5e0bcd5a6221bf17bcbb
maurici0123 <108777512+maurici0123@users.noreply.github.com>
2022-12-12 00:00:33 -0300

    atua des05

--- exercicios/ex009/idex.js
+++ exercicios/ex009/idex.js
@@ -1,6 +1,9 @@
 vetor = [4, 5, 2, 9, 7, 1]
 
-if (vetor.indexOf(1) == 5) {
+if (vetor.indexOf(9) == 3) {
     console.log('existe')
+}
+else {
+    console.log('n existe')
 }
 
---
+++ exercicios/ex009/script.js
@@ -0,0 +1,16 @@
+num = document.getElementById('txtn')
+res = document.getElementById('res')
+vet = []
+
+function vere() { 
+    
+    if (vet.indexOf(Number(num.value)) == -1) {
+        com = vet.length
+        vet.push(Number(num.value))
+        res.innerHTML += `<p>num ${vet[com]}</p>`
+
+    } else {
+        alert('nao')
+    }
+}
+
