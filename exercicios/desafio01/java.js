2dfb78d5cb2027e0ba21220255ed9562871e707e
maurici0123 <108777512+maurici0123@users.noreply.github.com>
2022-12-12 20:19:06 -0300

    atu des01

--- exercicios/desafio01/java.js
+++ exercicios/desafio01/java.js
@@ -2,16 +2,29 @@ img = document.getElementById('image')
 hora = document.getElementById('time')
 x = new Date()
 h = x.getHours()
+y = new Date()
+m = y.getMinutes()
 
-if (h >= 6 && h <= 12){
+if (h == 0) {
+
+    img.src= 'image/image-noite.jpg'
+    hora.innerHTML = `agora é meia noite e ${m} minutos.`
+}else if (h ==1) {
+
+    img.src= 'image/image-noite.jpg'
+    hora.innerHTML = `agora é ${h} hora e ${m} minutos.`
+}else if (h >= 6 && h <= 12){
+
     img.src= 'image/image-dia.jpg'
-    hora.innerHTML = `agora são ${h} horas.`
+    hora.innerHTML = `agora são ${h} horas e ${m} minutos.`
     document.body.style.background = '#9CB5DB'
 }else if (h > 12 && h <= 18){
+
     img.src= 'image/image-tarde.jpg'
-    hora.innerHTML = `agora são ${h} horas.`
+    hora.innerHTML = `agora são ${h} horas e ${m} minutos.`
     document.body.style.background = '#A54C3D'
 }else{
+
     img.src= 'image/image-noite.jpg'
-    hora.innerHTML = `agora são ${h} horas.`
+    hora.innerHTML = `agora são ${h} horas e ${m} minutos.`
 }
--- exercicios/ex009/idex.js
+++
@@ -1,9 +0,0 @@
-vetor = [4, 5, 2, 9, 7, 1]
-
-if (vetor.indexOf(9) == 3) {
-    console.log('existe')
-}
-else {
-    console.log('n existe')
-}
-
