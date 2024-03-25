5bfd521ef987de61d91f549737e913c35d825509
maurici0123 <108777512+maurici0123@users.noreply.github.com>
2024-03-25 09:02:02 -0300

    ajuste no cep

--- exercicios/react/cep/src/App.jsx
+++ exercicios/react/cep/src/App.jsx
@@ -6,40 +6,62 @@ const API = 'https://opencep.com/v1/'
 function App() {
   const [cep, setCep] = useState('');
   const [data, setData] = useState('')
-  const [display, setDisplay] = useState('none')
-  
-  const requisição =  () => {
-    if(cep != ''){
+  const [displayDate, setDisplayDate] = useState('none')
+  const [displayComple, setDisplayComple] = useState('none')
+
+  const requisição = () => {
+    if (cep != '') {
+
       fetch(API + cep)
-      .then(res => res.ok ? res.json() : '')
-      .then(data => setData(data), setDisplay('block'), setCep(''))
+
+        .then(r => {
+          if (r.ok != true) {
+            alert('cpf não encontrado!')
+            return
+
+          } else {
+
+            r.json()
+              .then((dados) => {
+                setData(dados)
+                setDisplayDate('block')
+
+                if (data.complemento) {
+                  setDisplayComple('block')
+                } else {
+                  setDisplayComple('none')
+                }
+              })
+          }
+        })
     }
+
   }
 
   return (
     <>
-    <div className='container'>
-      <input type="text" placeholder='CEP' required 
-      onChange={e => setCep(e.target.value)}/>
+      <div className='container'>
+        <input type="text" placeholder='CEP' required
+          onChange={e => setCep(e.target.value)} />
 
-      <button onClick={() => requisição()}>Buscar</button>
+        <button onClick={() => requisição()}>Buscar</button>
 
-      <div className='datas' style={{'display': display}}>
+        <div className='datas' style={{ 'display': displayDate }}>
 
-        <p> <h4>BAIRRO :</h4> {data.bairro ? ` ${data.bairro}` : ''}</p>
+          <p> <h4>BAIRRO :</h4> {data.bairro ? ` ${data.bairro}` : ''}</p>
 
-        <p> <h4>LOUGRADOURO :</h4> {data.logradouro ? ` ${data.logradouro}` : ''}</p>
+          <p> <h4>LOUGRADOURO :</h4> {data.logradouro ? ` ${data.logradouro}` : ''}</p>
 
-        <p> <h4>COMPLEMENTO :</h4> {data.complemento ? ` ${data.complemento}` : ''}</p>
+          <p style={{ 'display': displayComple }}> <h4>COMPLEMENTO :</h4> {data.complemento ? ` ${data.complemento}` : ''}</p>
 
-        <p>
-          <h4>LOCALIDADE :</h4>
-          <sapn>{` ${data.localidade}`}</sapn>
-          <span className='uf'>{data.uf}</span>
-        </p>
+          <p>
+            <h4>LOCALIDADE :</h4>
+            <sapn>{` ${data.localidade}`}</sapn>
+            <span className='uf'>{data.uf}</span>
+          </p>
 
+        </div>
       </div>
-    </div>
     </>
   )
 }
