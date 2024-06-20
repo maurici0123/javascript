92676259910b0fe0d93fef18eca4e73981197271
mauricio <mauriciosilvajunior12@gmail.com>
2024-06-20 13:48:22 -0300

    tsc 35

---
+++ exercicios/TypeScript/ex002/build/aula35.js
@@ -0,0 +1,14 @@
+"use strict";
+console.clear();
+let [a, b, c, d] = ['computador', 'teclado', 'mouse', 'monitor', 'mousepad'];
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+const obj = {
+    Nome: 'kauan',
+    Idade: 56
+};
+let { Nome, Idade } = obj;
+console.log(Nome);
+console.log(Idade);
