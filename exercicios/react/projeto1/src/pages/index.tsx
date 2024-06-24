c7323d95419ef71eecc99d3154fc9c429909a02b
mauricio <mauriciosilvajunior12@gmail.com>
2024-06-24 11:59:49 -0300

    react_step1

---
+++ exercicios/react/projeto1/src/pages/index.tsx
@@ -0,0 +1,13 @@
+//import Image from "next/image";
+// import { Inter } from "next/font/google";
+// const inter = Inter({ subsets: ["latin"] });
+
+const name='maria'
+
+export default function Home() {
+  return (
+    <main className=''>
+      <h1>{name}</h1>
+    </main>
+  );
+}
