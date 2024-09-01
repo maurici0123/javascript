cfc0b90762f96e1594d965636b544029c99cf53d
mauricio <mauriciosilvajunior12@gmail.com>
2024-09-01 18:37:43 -0300

    react style page 2

--- exercicios/react/chat_real_time/client/src/components/Chat/Chat.jsx
+++ exercicios/react/chat_real_time/client/src/components/Chat/Chat.jsx
@@ -4,6 +4,7 @@ import { VscSend } from "react-icons/vsc";
 
 export default function Chat(props) {
 
+    const bottomRef = useRef()
     const messageRef = useRef()
     const [messageList, setMessageList] = useState([])
 
@@ -17,6 +18,10 @@ export default function Chat(props) {
         }
     }, [props.socket])
 
+    useEffect(() => {
+        scrollDown()
+    }, [messageList])
+
     const handleSubmit = () => {
         const message = messageRef.current.value
         if (!message.trim()) return
@@ -29,13 +34,19 @@ export default function Chat(props) {
     const clearInput = () => {
         messageRef.current.value = ''
     }
+
     const focusInput = () => {
         messageRef.current.focus()
     }
+
     const getEnterKey = (e) => {
         if (e.code === 'Enter') {
             handleSubmit()
         }
+    }
+
+    const scrollDown = () => {
+        bottomRef.current.scrollIntoView()
     }
 
     return (
@@ -44,14 +55,23 @@ export default function Chat(props) {
                 <div className='conversation'>
                     {
                         messageList.map((message, index) => (
-                            <p key={index} >{message.author}: {message.text}</p>
+                            <div
+                                className={`menssage-area ${message.authorId === props.socket.id ? 'my-message-area' : 'other-message-area'}`} key={index}>
+
+                                <div className={`menssage ${message.authorId === props.socket.id ? 'my-message' : 'other-message'}`}>
+                                    <p className={`author ${message.authorId === props.socket.id ? 'my-author' : 'other-author'}`}>{message.author}:</p>
+
+                                    {message.text}
+                                </div>
+                            </div>
                         ))
                     }
+                    <div ref={bottomRef} />
                 </div>
 
                 <div className='input-area'>
                     <input type="text" className='send-input' ref={messageRef} onKeyDown={e => getEnterKey(e)} placeholder='Mensagem' />
-                    <button className='send-button' onClick={() => handleSubmit()}><VscSend className='send-icon'/></button>
+                    <button className='send-button' onClick={() => handleSubmit()}><VscSend className='send-icon' /></button>
                 </div>
             </div>
         </div>
