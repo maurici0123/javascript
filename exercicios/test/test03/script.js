ff9c06c1dce4dbb2ad6f73d7097fca5d2199c214
mauricio <mauriciosilvajunior12@gmail.com>
2024-10-18 12:54:40 -0300

    react-chat/criando sistema de armazenamento de imagens

--- exercicios/test/test03/script.js
+++ exercicios/test/test03/script.js
@@ -1,5 +1,19 @@
-v = document.getElementById('d')
+const openFileExplorerButton = document.querySelector('button'); // Botão para abrir o explorador
 
-function show() {
-    console.log(v.innerHTML)
-}
+openFileExplorerButton.addEventListener('click', () => {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    
+    fileInput.addEventListener('change', (event) => {
+        //console.log(event.target.files[0])
+
+        const file = event.target.files[0];
+        console.log(file);
+        
+        const blobURL = URL.createObjectURL(file)
+        console.log(blobURL)
+        //document.querySelector('img').src = blobURL; // Exibe a imagem selecionada
+    });
+    
+    fileInput.click();
+});
